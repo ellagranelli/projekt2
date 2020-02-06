c3c610a5ad4eef9a58bfce34114b466663863e6a
Ella Granelli <ella.granelli@elev.ga.ntig.se>
2020-02-06 12:04:54 +0100

    skapat en underflik och gjort färdigt menyn

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+    
+    document.querySelector('.navLinks').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
+}
+
+document.querySelector('.burger').addEventListener('click', toggleMeny)
