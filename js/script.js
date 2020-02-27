fe27f6111a1915fa306308063a9f53b5e277c891
Ella Granelli <ella.granelli@elev.ga.ntig.se>
2020-02-27 12:12:38 +0100

    Fortsatt med html och CCS, lagt till fler funktioner på sidan

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ function toggleMeny() {
 }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
+
+const box = document.querySelector('.notificationsBox')
+const button = document.querySelector('.notifications')
+
+function toggleMeny() {
+    document.querySelector('.notificationsBox').classList.toggle('navActive')
+}
+
+document.querySelector('.notifications').addEventListener('click', toggleMeny)
+
+//Modal 
+var modal = document.getElementById("myModal");
+
+var btn = document.querySelector(".plus");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
