3a76d03fbd4c53c8626ab0d443d8d8184ba94806
Ella Granelli <ella.granelli@elev.ga.ntig.se>
2020-03-13 11:02:39 +0100

    js

--- js/script.js
+++ js/script.js
@@ -14,10 +14,9 @@ const button = document.querySelector('.notifications')
 
 function toggleBox() {
       document.querySelector('.notificationBox').classList.toggle('navActive')
-      document.querySelector('.notifications').classList.toggle('toggle')
 }
 
-document.querySelector('.notifications').addEventListener('click', toggleMeny)
+document.querySelector('.notifications').addEventListener('click', toggleBox)
 
 //Modal 
 var modal = document.getElementById("myModal");
@@ -39,31 +38,36 @@ window.onclick = function(event) {
   }
 }
 
-function AddReminder() {
+///stopp
+
+function AddReminder(title,time,day,month,year) {
 
         let box = document.createElement('div');
         box.innerHTML = `
-    <p>${newreminder.title}</p>
-    <p>${newreminder.time}</p>
-    <p>${newreminder.day}</p>
-    <p>${newreminder.month}</p>
-    <p>${newreminder.year}</p>
+    <p>${title}</p>
+    <p>${time}</p>
+    <p>${day}</p>
+    <p>${month}</p>
+    <p>${year}</p>
         `;
+        console.log(box);
         document.querySelector('.instruktion').style.display = "none";
-        document.querySelector('.text').style.display = ""
-        document.querySelector('.påminnelse').appendChild(row);
+        document.querySelector('.myReminder').style.display = "flex";
+        document.querySelector('.reminder').style.display = "flex"; 
+        document.querySelector('.reminder').appendChild(box);
+        modal.style.display = "none";
+  
     }
 
 document.querySelector('#myModal').addEventListener('submit', function(e) {
+  e.preventDefault();
     let title = document.querySelector('#titel').value;
     let time = document.querySelector('#deadline-hours').value;
     let day = document.querySelector('#deadline-day').value;
     let month = document.querySelector('#deadline-month').value;
     let year = document.querySelector('#deadline-year').value;
 
-let newreminder = new reminder(title,time,day,month,year);
+AddReminder(title,time,day,month,year);
 
-newreminder.AddReminder(newreminder);
 
-e.preventDefault();
 });
